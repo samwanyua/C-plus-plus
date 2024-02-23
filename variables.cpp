970fd42aded850259edaaa53f14afdff649a87de
samwanyua <samexample8@gmail.com>
2024-02-23 13:57:09 +0300

    Variables

---
+++ variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Variables
+    string characterName = "Sam";
+   
+    int characterAge;
+    characterAge = 25;
+
+
+
+    cout << "There was a man named "<< characterName << endl;
+    cout << "He was " << characterAge <<" years old." << endl; 
+
+    characterName = "Tom";
+    characterAge = 47;
+    cout << "He liked the name "<< characterName << endl;
+    cout << "But did not like being "<< characterAge <<" !" << endl;
+
+
+
+    return 0;
+}
