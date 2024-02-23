520131a4374c0dd91a084cc056b936ce6c9b516b
samwanyua <samexample8@gmail.com>
2024-02-23 23:41:35 +0300

    Data types

---
+++ dataTypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Data types
+    char grade = 'A';
+
+    string userName = "Giraffe Academy";
+
+    int age = 50; //whole numbers
+
+    float gpa; //Typically 4 bytes (32 bits). Approximately ±3.4e±38 (7 decimal digits of precision).
+
+    double newGpa; //Typically 8 bytes (64 bits).  (15 decimal digits of precision).
+
+    //boolean - true or false
+    bool isMale = true;
+
+    cout << grade << endl;
+    cout << "This is c++" << endl; //this is a constant
+
+    return 0;
+}
