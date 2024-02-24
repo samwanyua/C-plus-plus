34e4c57b30131efee0ce7739d9f85bcfede74182
samwanyua <samexample8@gmail.com>
2024-02-24 07:43:02 +0300

    Basic Calculator

---
+++ simple_calc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // int num1;
+    // int num2;
+
+    double num1,num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1; 
+
+    cout << "Enter the second number: ";
+    cin >> num2; 
+
+    cout << num1 + num2 << endl;
+
+
+
+    return 0;
+}
