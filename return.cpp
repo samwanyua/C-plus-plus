dc9a8727ec2974f2b64bb593f244bfad25fc79c4
samwanyua <samexample8@gmail.com>
2024-02-24 08:25:24 +0300

    Return statements

---
+++ return.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+double toCube(double x){
+    // cout << "Hello" << endl;
+    return x * x * x;
+}
+
+int main(){
+    //return statements
+
+    double answer = toCube(3232);
+    cout << answer << endl;
+
+
+
+    return 0;
+}
