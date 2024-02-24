e919e5422037577a9abb5b51e7107ace73c77c93
samwanyua <samexample8@gmail.com>
2024-02-24 08:04:47 +0300

    Arrays

---
+++ arrays.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //arrays - container for multiple data values
+    //variables - stores only one value
+
+    int luckNums[] = {2,4,5,6,7,8,9,12};
+
+    cout << luckNums[2] << endl; //accessing individual element
+
+    //modify
+    luckNums[0] = 200;
+     cout << luckNums[0] << endl;
+
+    
+
+
+
+    return 0;
+}
