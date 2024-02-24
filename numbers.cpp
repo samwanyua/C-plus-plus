f980ee47e1e0f8b5cc879d009c4aa7d05157bbfe
samwanyua <samexample8@gmail.com>
2024-02-24 07:03:05 +0300

    Working withnumbers

---
+++ numbers.cpp
@@ -0,0 +1,51 @@
+#include <iostream> //input output function
+#include <cmath> //math functions
+
+using namespace std;
+
+int main() {
+
+    //Working with  numbers
+
+
+    cout << 40 + 239 << endl ;
+    cout << 10 % 3 << endl ;
+    //order of operation PEMDAS (Parenthesis Exponents Multiplication Division Addition Subtraction) Please Excuse My Dear Aunt Sally
+
+    int wnum = 5;
+    wnum ++;
+
+    double dnum = 4.232;
+
+    cout << wnum << endl;
+
+    cout << 4.23 + 4 << endl; //8.23
+
+    cout << 10 / 3.0 << endl; // 3.33333
+
+    cout << pow(2,5) << endl; // 32
+
+    cout << sqrt(36) << endl; //6
+
+    cout << round(4.233) << endl; //4
+
+    cout << ceil (3.23) << endl; //4
+
+    cout << floor(23.879) << endl; //23 rounding down
+
+    cout << fmax(23,34) << endl; //34 return the max
+
+    cout << fmin (23,34) << endl; //23 return the min
+
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
