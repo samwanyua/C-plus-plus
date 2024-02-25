fa11e8c76090dabddbd18e995c25a2650794904c
samwanyua <samexample8@gmail.com>
2024-02-25 03:40:39 +0300

    While loops and Do while loop

--- while_loops.cpp
+++ while_loops.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int main(){
 
     //while loops - looping through a condition as long as a condition is true
-    int index = 1;
+    int index = 6;
 
-    while(index <= 5 ){
+    // while(index <= 5 ){
+    //     cout << index << endl;
+    //     index ++;
+    // }
+
+    do {
         cout << index << endl;
         index ++;
-    }
+    } while(index <= 5 );
 
 
     return 0;
