a294b5cda22133c86f12ccec67dda5e3dea87039
samwanyua <samexample8@gmail.com>
2024-02-25 03:37:49 +0300

    While loops

---
+++ while_loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //while loops - looping through a condition as long as a condition is true
+    int index = 1;
+
+    while(index <= 5 ){
+        cout << index << endl;
+        index ++;
+    }
+
+
+    return 0;
+}
